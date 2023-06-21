5e883fe2def29c5b5a559e9090b68df2d25a0467
Timo <tim.kras1@mail.ru>
2023-06-21 22:05:27 +0500

    why not? error:HP0/100

---
+++ Lesson 5 module 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename Swap>
+Swap swap(Swap x, Swap y) {
+    cout << y <<"\n";
+    return x;
+}
+template<typename Info>
+Info info(Info x) {
+    cout << typeid(x).name() << endl;
+    return x;
+}
+
+int main()
+{
+    cout << swap("1", "two") << endl;
+    cout << swap("first", "second") << endl;
+    cout << info("1");
+};
